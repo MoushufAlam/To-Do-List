e5767c9f940c039ee2ad466cfb3ab7b58f22b53a
MoushufAlam <rishu78698@gmail.com>
2025-02-11 20:10:57 +0530

    Project Completed

--- Base/src/App.jsx
+++ Base/src/App.jsx
@@ -1,21 +1,19 @@
 import { useEffect, useState } from "react";
-import { TodoProvider } from "../Context/ToDoContext";
+import { TodoProvider } from "./Context/ToDoContext";
+import TodoForm from "./Components/TodoForm";
 import "./App.css";
+import TodoItem from "./Components/TodoItem";
 
 function App() {
   const [todos, setTodos] = useState([]);
 
   const addTodo = (todo) => {
-    setTodos((prev) => {
-      [{ id: Date.now(), ...todo }, ...prev];
-    });
+    setTodos((prev) => [{ id: Date.now(), ...todo }, ...prev]);
   };
 
   const updateTodo = (id, todo) => {
     setTodos((prev) =>
-      prev.map((prevTodo) => {
-        prevTodo.id === id ? todo : prevTodo;
-      })
+      prev.map((prevTodo) => (prevTodo.id === id ? todo : prevTodo))
     );
   };
 
@@ -24,19 +22,25 @@ function App() {
   };
 
   useEffect(() => {
-    const todos = JSON.parse(localStorage.getItem("todos"))
-    if(todos & todos.length>0){
+    const todos = JSON.parse(localStorage.getItem("todos"));
+    if (todos && todos.length > 0) {
       setTodos(todos);
     }
-  },[])
+  }, []);
 
   useEffect(() => {
-    localStorage.setItem("todos" , JSON.stringify(todos))
-  },[todos])
+    localStorage.setItem("todos", JSON.stringify(todos));
+  }, [todos]);
 
   const toggleComplete = (id) => {
-    setTodos((prev) => prev.map((prevTodo)=>  prevTodo  === id ? {...prevTodo, completed: !prevTodo.completed} : prevTodo))
-  }
+    setTodos((prev) =>
+      prev.map((prevTodo) =>
+        prevTodo.id === id
+          ? { ...prevTodo, completed: !prevTodo.completed }
+          : prevTodo
+      )
+    );
+  };
 
   return (
     <TodoProvider
@@ -47,9 +51,16 @@ function App() {
           <h1 className="text-2xl font-bold text-center mb-8 mt-2">
             Manage Your Todos
           </h1>
-          <div className="mb-4">{/* Todo form goes here */}</div>
+          <div className="mb-4">
+            <TodoForm />
+          </div>
           <div className="flex flex-wrap gap-y-3">
             {/*Loop and Add TodoItem here */}
+            {todos.map((todo) => (
+              <div key={todo.id} className="w-full">
+                <TodoItem todo={todo} />
+              </div>
+            ))}
           </div>
         </div>
       </div>
